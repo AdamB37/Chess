b75abe4c987b1511dd1aaddb0058a8194e660a78
AdamB37 <adam.beshir@gmail.com>
2017-05-08 15:52:02 -0700

    complete grid with chess pieces

---
+++ globalImports.js
@@ -0,0 +1,30 @@
+import {
+  Grid,
+  Col,
+  Row
+} from 'react-bootstrap'
+
+import React from 'react'
+
+import { render } from 'react-dom'
+
+import {
+  Link,
+  Route,
+  HashRouter,
+  Switch
+} from 'react-router-dom'
+
+import 'react-router'
+
+module.exports = {
+  Col,
+  Grid,
+  HashRouter,
+  Link,
+  React,
+  render,
+  Route,
+  Row,
+  Switch
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+import { React, render } from './globalImports'
+
+import App from './public/app'
+
+render (
+  <App />,
+  document.getElementById("mount")
+)
